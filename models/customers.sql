7862a1fc80ed7b5c057d2318dc63295698253fac
Lucas Andrade <lucasandradesilva753@gmail.com>
2024-08-02 15:30:11 -0300

    alterações no customers

--- models/customers.sql
+++ models/customers.sql
@@ -1,6 +1,12 @@
+WITH markup {
 select *
 ,first_value(customer_id) 
 over(partition by company_name, contact_name 
 order by company_name
 rows between unbounded preceding and unbounded following) as result
 From {{ source('sources', 'customers') }}
+}, removed {
+    Select distinct result FROM markup
+}
+
+select * from removed
