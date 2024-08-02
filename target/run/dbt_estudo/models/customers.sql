c92ce1168978a92ff734211a6a2c365fadb02cc0
Lucas Andrade <lucasandradesilva753@gmail.com>
2024-08-02 12:18:57 -0300

    alterações no customers

--- target/run/dbt_estudo/models/customers.sql
+++ target/run/dbt_estudo/models/customers.sql
@@ -1,10 +1,18 @@
 
+  
+    
 
-  create view "northwind"."dbt_landradeesilva"."customers__dbt_tmp" as (
+  create  table
+    "northwind"."dbt_landradeesilva"."customers__dbt_tmp"
+    
+    
+    
+  as (
     select *
 ,first_value(customer_id) 
 over(partition by company_name, contact_name 
 order by company_name
 rows between unbounded preceding and unbounded following) as result
 From "northwind"."public"."customers"
-  ) ;
+  );
+  
