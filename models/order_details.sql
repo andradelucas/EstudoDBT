4b467c7ea0aac932b7b831682fe150d2dc1c3840
Lucas Andrade <lucasandradesilva753@gmail.com>
2024-08-02 19:07:02 -0300

    alterações no order_details

---
+++ models/order_details.sql
@@ -0,0 +1,9 @@
+WITH temp as(
+SELECT
+od.order_id, od.product_id, od.unit_price, pr.product_name, pr.supplier_id, pr.category_id
+,od.unit_price * od.quantity total
+,(pr.unit_price * od.quantity) - total discount
+from{{source('sources','order_details')}} od
+left join  {{source('sources','products')}} pr on (od.product_id = pr.product_id)
+)
+select * FROM temp
---
+++ target/run/dbt_estudo/models/order_details.sql
@@ -0,0 +1,21 @@
+
+  
+    
+
+  create  table
+    "northwind"."dbt_landradeesilva"."order_details__dbt_tmp"
+    
+    
+    
+  as (
+    WITH temp as(
+SELECT
+od.order_id, od.product_id, od.unit_price, pr.product_name, pr.supplier_id, pr.category_id
+,od.unit_price * od.quantity total
+,(pr.unit_price * od.quantity) - total discount
+from"northwind"."public"."order_details" od
+left join  "northwind"."public"."products" pr on (od.product_id = pr.product_id)
+)
+select * FROM temp
+  );
+  
