821212077e5cb0484976abae68a63222273b6e74
Lucas Andrade <lucasandradesilva753@gmail.com>
2024-08-06 08:53:34 -0300

    catreport criacao

---
+++ target/compiled/dbt_estudo/models/catreport.sql
@@ -0,0 +1,1 @@
+SELECT * FROM "northwind"."public"."categories"
---
+++ target/run/dbt_estudo/models/catreport.sql
@@ -0,0 +1,13 @@
+
+  
+    
+
+  create  table
+    "northwind"."dbt_landradeesilva"."catreport__dbt_tmp"
+    
+    
+    
+  as (
+    SELECT * FROM "northwind"."public"."categories"
+  );
+  
